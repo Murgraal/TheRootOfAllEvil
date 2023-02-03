b64ea2856cb337317442338330cf3e9267040f84
Kim Ordén <murgrandalf@Kims-MacBook-Air.local>
2023-02-03 22:49:34 +0200

    Added data generation base

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -4,7 +4,15 @@ public static class Data
 {
     public static int Health;
     public static string SourceString;
+    public static List<MovingCharacterData> GeneratedCharacterData;
 
+    public static List<MovingCharacterData> GenerateCharacterData()
+    {
+        var result = new List<MovingCharacterData>();
+
+        return result;
+    }
+    
     public static Queue<MovingCharacterData> CharacterQueue;
     
     public static class GamePlay
