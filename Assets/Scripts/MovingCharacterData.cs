94bedb014b35015faca081ee406abd192c0faf96
Samuel Zelenka <samuel.zelenka@hotmail.com>
2023-02-03 22:48:27 +0200

    moving character constructor added

--- Assets/Scripts/MovingCharacterData.cs
+++ Assets/Scripts/MovingCharacterData.cs
@@ -1,7 +1,16 @@
+using System;
 using UnityEngine;
 
 public class MovingCharacterData
 {
+    public MovingCharacterData(char character)
+    {
+        Letter = character;
+        if (!Enum.TryParse<KeyCode>(Letter.ToString(), true, out ValidKey))
+        {
+            Debug.LogWarning($"Failed to parse keycode {Letter}");
+        }
+    }
     public KeyCode ValidKey;
     public char Letter;
     public int positionInString;
